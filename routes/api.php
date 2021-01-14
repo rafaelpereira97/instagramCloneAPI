cde58babd9b85ab17aa1ea892d5f869edb06ee11
Rafael <rafaelpereira_97@outlook.com>
2021-01-14 00:14:14 +0000

    Route group

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,13 +19,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login',[\App\Http\Controllers\ApiController::class,'login']);
-Route::post('/register',[\App\Http\Controllers\ApiController::class,'register']);
-Route::get('/checkUser/{usernameInput}',[\App\Http\Controllers\ApiController::class,'checkUser']);
+Route::post('/login',[ApiController::class,'login']);
+Route::post('/register',[ApiController::class,'register']);
+Route::get('/checkUser/{usernameInput}',[ApiController::class,'checkUser']);
 
-Route::post('/insertPost',[\App\Http\Controllers\ApiController::class,'insertPost'])->middleware('auth:api');
-Route::get('/getFeed',[\App\Http\Controllers\ApiController::class,'getFeed']);
+Route::get('/getFeed',[ApiController::class,'getFeed']);
 
-Route::post('/makeComment',[\App\Http\Controllers\ApiController::class,'makeComment'])->middleware('auth:api');
-Route::get('/profile/{username}',[\App\Http\Controllers\ApiController::class,'getMyProfileUser']);
-Route::get('/profile/{username}/Posts',[\App\Http\Controllers\ApiController::class,'getMyProfile']);
+Route::get('/profile/{username}',[ApiController::class,'getMyProfileUser']);
+Route::get('/profile/{username}/Posts',[ApiController::class,'getMyProfile']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/makeComment',[ApiController::class,'makeComment'])->middleware('auth:api');
+    Route::post('/insertPost',[ApiController::class,'insertPost'])->middleware('auth:api');
+});
